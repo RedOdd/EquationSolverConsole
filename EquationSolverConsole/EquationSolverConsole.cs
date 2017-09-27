cafbbc0852ed4a433ce078bc5f2e3f7affa9f269
RedOdd <redodd@yandex.ru>
2017-09-27 23:21:59 +0300

    EquationSolverConsole

---
+++ EquationSolverConsole/EquationSolverConsole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EquationSolverNamespace;
+
+namespace EquationSolverConsole
+{
+    class EquationSolverConsole
+    {
+        static void Main(string[] args)
+        {
+            var equation = Console.ReadLine();
+            var coefficients = EquationSolver.ParseEquation(equation);
+            var discriminat = EquationSolver.FindDiscriminant(coefficients);
+            if (EquationSolver.IsValidationOfDiscriminantIsTrue(discriminat))
+            {
+                var firstValue = EquationSolver.FindFirstValue(coefficients, discriminat);
+                var secondValue = EquationSolver.FindSecondValue(coefficients, discriminat);
+                var arrayOfValues = EquationSolver.ViewTwoValues(firstValue, secondValue);
+                Console.WriteLine("Value: " + arrayOfValues[0]);
+                if (!(arrayOfValues[0] == arrayOfValues[1]))
+                {
+                    Console.Write("Value: " + arrayOfValues[1]);
+                }
+                Console.ReadKey();
+                
+            } else
+            {
+                Console.Write("Error");
+                Console.ReadKey();
+            }
+        }
+    }
+}
